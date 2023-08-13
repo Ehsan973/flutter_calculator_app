faa68ddb7ed90896dce3898976b0bd5eafb7a2af
Ehsan973 <81531659+Ehsan973@users.noreply.github.com>
2023-08-13 18:06:28 +0330

    Initial Application

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:calculator_app/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
