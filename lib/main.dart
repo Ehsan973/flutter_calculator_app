bbb1b177ce91288a236834ba0db6dad05cd5648b
Ehsan973 <81531659+Ehsan973@users.noreply.github.com>
2023-08-13 19:28:47 +0330

    Add operands and operators TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,59 @@ void main() {
 class Application extends StatelessWidget {
   const Application({super.key});
 
+  Widget getRow(String text1, String text2, String text3, String text4) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        TextButton(
+          onPressed: () {},
+          child: Padding(
+            padding: EdgeInsets.all(3),
+            child: Text(
+              text1,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 26, color: Colors.grey),
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Padding(
+            padding: EdgeInsets.all(3),
+            child: Text(
+              text2,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 26, color: Colors.grey),
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Padding(
+            padding: EdgeInsets.all(3),
+            child: Text(
+              text3,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 26, color: Colors.grey),
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          style: ButtonStyle(),
+          child: Padding(
+            padding: EdgeInsets.all(3),
+            child: Text(
+              text4,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 26, color: Colors.grey),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +68,28 @@ class Application extends StatelessWidget {
       home: Scaffold(
         body: Column(
           children: [
-            
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: backgroundGreyDark,
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: Container(
+                color: backgroundGrey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    getRow('ac', 'ce', '%', '/'),
+                    getRow('1', '2', '3', '*'),
+                    getRow('4', '5', '6', '-'),
+                    getRow('7', '8', '9', '+'),
+                    getRow('00', '0', '.', '='),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
